01d8f6241eae52ec1d29ee6f840202b0205f85ba
Vladislav <vlad.savko21@gmail.com>
2022-09-01 22:20:56 +0300

    adding routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Route, Navigate, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import Layout  from './components/Layout'
+import React from 'react';
+import CoinInfo from './components/CoinInfo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path='/' element={<Home />} />
+          <Route path="/coin/:id" element={<CoinInfo />} />
+          <Route path='*' element={<Navigate replace to="/" />} />
+        </Route>
+      </Routes>
   );
 }
 
