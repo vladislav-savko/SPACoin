b0b42bccc8d9cb3bd8a31dd33d9795eccba9e9ef
Vladislav <vlad.savko21@gmail.com>
2022-09-03 03:21:16 +0300

    readable code, add component 'buttons'

---
+++ src/components/Button/index.js
@@ -0,0 +1,11 @@
+import './index.scss'
+
+const Button = ({textButton}) => {
+    return (
+        <div className='button'>
+            { textButton }
+        </div>
+    )
+}
+
+export default Button
