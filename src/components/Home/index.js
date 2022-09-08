630f1091a4fc6468084a7c9526293319ea00692d
Vladislav <vlad.savko21@gmail.com>
2022-09-08 10:41:39 +0300

    added pagination to home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,9 +4,13 @@ import { createAPI } from '../../api'
 import TableCoins from '../TableCoins'
 
 import { useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
 
 const Home = () => {
     const [data, setData] = useState([]);
+    const [itemOffset, setItemOffset] = useState([]);
+
+    const [totalCountPage, setTotalCountPage] = useState(0);
 
     useEffect(() => {
         createAPI('v2/assets').fetch().then(resp => {
@@ -14,14 +18,32 @@ const Home = () => {
         });
     }, [setData]);
 
+    useEffect(() => {
+        setTotalCountPage(data.length/20);
+        setItemOffset(data.slice(0, 20));
+    }, [data]);
+
+    const handlePageClick = (event) => {
+        setItemOffset(data.slice(event.selected*20, event.selected*20+20));
+    };
+
     return (
-        <>
         <div className='home'>
             <div className='home__wrapper'>
-                <TableCoins coins={data} />
+                <TableCoins coins={itemOffset} />
+
+                <ReactPaginate  
+                    breakLabel=""
+                    nextLabel=">"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={5}
+                    pageCount={totalCountPage}
+                    previousLabel="<"
+                    renderOnZeroPageCount={null}
+                    className="home__pagination"
+                />
             </div>
         </div>  
-      </>
     )
 }
 
